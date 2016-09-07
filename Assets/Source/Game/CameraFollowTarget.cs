dabdf22401909bb84b02e347a2b12243d02a8eee
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-09-07 17:25:09 +0200

    Camera movement.

---
+++ Assets/Source/Game/CameraFollowTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowTarget : MonoBehaviour
+{
+    public const float FOLLOW_SPEED = 3.0f;
+    [SerializeField]
+    private Transform target;
+
+	void LateUpdate () 
+    {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position;
+            targetPosition.z = transform.position.z;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * FOLLOW_SPEED);
+        }
+	}
+}
