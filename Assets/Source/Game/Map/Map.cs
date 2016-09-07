c4cd127f068f4964d7c2664133a9fa3bfe29cb6d
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-09-07 12:39:10 +0200

    First commit.

---
+++ Assets/Source/Game/Map/Map.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+    public const float MAP_WIDTH = 25;
+    public const float MAP_HEIGHT = 25;
+    public const float COLLIDER_THICKNESS = 0.2f;
+
+    void Start()
+    {
+        
+    }
+
+//    private void GenerateBorders()
+//    {
+//        // Left
+//        GameObject left = new GameObject("Left Border");
+//        left.transform.position = new Vector3(-MAP_WIDTH / 2 - COLLIDER_THICKNESS / 2, 0, 0);
+//        left.transform.SetParent(transform, true);
+//        BoxCollider2D leftCollider = left.AddComponent<BoxCollider2D>();
+//        left.transform.localScale = new Vector3(COLLIDER_THICKNESS, MAP_HEIGHT, 1);
+//
+//        // Right
+//        GameObject right = new GameObject("Right Border");
+//        right.transform.position = new Vector3(MAP_WIDTH / 2 + COLLIDER_THICKNESS / 2, 0, 0);
+//        right.transform.SetParent(transform, true);
+//        BoxCollider2D rightCollider = right.AddComponent<BoxCollider2D>();
+//        rightCollider.size = new Vector3(COLLIDER_THICKNESS, MAP_HEIGHT, 0);
+//
+//        // Top
+//        GameObject top = new GameObject("Top Border");
+//        top.transform.position = new Vector3(0, MAP_HEIGHT / 2 + COLLIDER_THICKNESS / 2, 0);
+//        top.transform.SetParent(transform, true);
+//        BoxCollider2D topCollider = top.AddComponent<BoxCollider2D>();
+//        topCollider.size = new Vector3(MAP_WIDTH + COLLIDER_THICKNESS * 2, COLLIDER_THICKNESS, 0);
+//
+//        // Bottom
+//        GameObject bottom = new GameObject("Bottom Border");
+//        bottom.transform.position = new Vector3(0, -MAP_HEIGHT / 2 - COLLIDER_THICKNESS / 2, 0);
+//        bottom.transform.SetParent(transform, true);
+//        BoxCollider2D bottomCollider = bottom.AddComponent<BoxCollider2D>();
+//        bottomCollider.size = new Vector3(MAP_WIDTH + COLLIDER_THICKNESS * 2, COLLIDER_THICKNESS, 0);
+//
+//    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(MAP_WIDTH, MAP_HEIGHT, 0.1f));
+    }
+}
---
+++ Assets/Source/UI/TouchInput.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField]
+    private RectTransform joystickBackground;
+
+    [SerializeField]
+    private RectTransform joystick;
+
+    private int joystickFingerId;
+    private bool isJoystickActive;
+    private Vector2 touchStart;
+    private float joystickMaxDistance;
+
+    void Start()
+    {
+        HideJoystick();
+
+        // Calculate distance using world background corner positions. It should look same witch any resolution.
+        Vector3[] corners = new Vector3[4];
+        joystickBackground.GetWorldCorners(corners);
+        float worldWidth = corners[2].x - corners[0].x;
+        joystickMaxDistance = worldWidth / 2;
+    }
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        if (isJoystickActive == false)
+        {
+            isJoystickActive = true;
+            joystickFingerId = eventData.pointerId;
+            SetAxisValue(Vector2.zero);
+            ShowJoystick(eventData.position);
+        }
+    }
+
+    public void OnPointerUp (PointerEventData eventData)
+    {
+        if (isJoystickActive == true && joystickFingerId == eventData.pointerId)
+        {
+            isJoystickActive = false;
+            SetAxisValue(Vector2.zero);
+            HideJoystick();
+        }
+    }
+
+    public void OnDrag (PointerEventData eventData)
+    {
+        if (isJoystickActive == true && joystickFingerId == eventData.pointerId)
+        {
+            Vector2 dir = eventData.position - touchStart;
+            if (dir.magnitude > GameInput.INPUT_DEAD_ZONE)
+            {
+                SetAxisValue(dir.normalized);
+            }
+            else
+            {
+                SetAxisValue(Vector2.zero);
+            }
+
+            UpdateJoystickPosition(eventData.position);
+        }
+    }
+
+    private void SetAxisValue(Vector2 axis)
+    {
+        GameInput.Axis = axis;
+    }
+
+    private void ShowJoystick(Vector2 start)
+    {
+        touchStart = start;
+        joystickBackground.position = start;
+        joystick.localPosition = Vector3.zero;
+        joystickBackground.gameObject.SetActive(true);
+    }
+
+    private void HideJoystick()
+    {
+        joystickBackground.gameObject.SetActive(false);
+    }
+
+    private void UpdateJoystickPosition(Vector2 current)
+    {
+        joystick.position = Vector2.MoveTowards(joystickBackground.position, current, joystickMaxDistance);
+    }
+}
+
+
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+//using System.Collections;
+//
+//public class TouchInput : MonoBehaviour
+//{
+//    private const int NO_FINGER_ID = -1;
+//
+//    [SerializeField]
+//    private RectTransform joystickBackground;
+//
+//    [SerializeField]
+//    private RectTransform joystick;
+//
+//    [SerializeField]
+//    private Text label; //TODO test only
+//
+//    private int joystickFingerId = NO_FINGER_ID;
+//    private bool isJoystickActive;
+//    private Vector2 touchStart;
+//
+//    void Update()
+//    {
+//        foreach (var touch in Input.touches)
+//        {
+//            if (touch.phase == TouchPhase.Began)
+//            {
+//                OnFingerDown(touch.fingerId, touch.position);
+//            }
+//            else if (touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended)
+//            {
+//                OnFingerUp(touch.fingerId, touch.position);
+//            }
+//            else if (touch.phase == TouchPhase.Moved)
+//            {
+//                OnFingerMove(touch.fingerId, touch.position);
+//            }
+//        }
+//    }
+//
+//    protected void OnFingerDown(int fingerId, Vector2 position)
+//    {
+//        if (isJoystickActive == false && !UiCollisionTest())
+//        {
+//            isJoystickActive = true;
+//            joystickFingerId = fingerId;
+//            SetAxisValue(Vector2.zero);
+//            SetStartPosition(position);
+//        }
+//    }
+//
+//    protected void OnFingerUp(int fingerId, Vector2 position)
+//    {
+//        if (isJoystickActive == true && joystickFingerId == fingerId)
+//        {
+//            isJoystickActive = false;
+//            joystickFingerId = NO_FINGER_ID;
+//            SetAxisValue(Vector2.zero);
+//        }
+//    }
+//
+//    protected void OnFingerMove(int fingerId, Vector2 position)
+//    {
+//        if (isJoystickActive == true && joystickFingerId == fingerId)
+//        {
+//            Vector2 dir = position - touchStart;
+//            if (dir.magnitude > GameInput.INPUT_DEAD_ZONE)
+//            {
+//                SetAxisValue(dir.normalized);
+//            }
+//            else
+//            {
+//                SetAxisValue(Vector2.zero);
+//            }
+//
+//            UpdateJoystickPosition(position);
+//        }
+//    }
+//
+//    private void SetAxisValue(Vector2 axis)
+//    {
+//        label.text = "Axis: " + axis;
+//        GameInput.Axis = axis;
+//    }
+//
+//    private void SetStartPosition(Vector2 start)
+//    {
+//        touchStart = start;
+//        joystickBackground.position = start;
+//        joystick.localPosition = Vector3.zero;
+//    }
+//
+//    private void UpdateJoystickPosition(Vector2 current)
+//    {
+//        joystick.position = Vector2.MoveTowards(joystickBackground.position, current, joystickBackground.rect.width);
+//    }
+//
+//    /*
+//     * If true, UI was clicked.
+//     */
+//    public bool UiCollisionTest()
+//    {
+//        return EventSystem.current.IsPointerOverGameObject();
+//    }
+//}
