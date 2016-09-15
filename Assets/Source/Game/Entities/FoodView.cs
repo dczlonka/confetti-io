724651572474ab228b1f4d48e8d045fcb1386d20
Dawid Członka <dawid.czlonka@pixzell.pl>
2016-09-15 15:00:03 +0200

    Random food color.

--- Assets/Source/Game/Entities/FoodView.cs
+++ Assets/Source/Game/Entities/FoodView.cs
@@ -1,6 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FoodView : EntityView {
-    
+public class FoodView : EntityView
+{
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        // Pick a random, saturated and not-too-dark color
+        sprite.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
 }
